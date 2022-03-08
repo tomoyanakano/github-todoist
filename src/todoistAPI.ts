fa28358e8c07e7370245e868a7aa77c1dbbc8e9d
tomotomy <tomo1999724@gmail.com>
2022-03-08 14:42:48 +0900

    :bug: fix url

--- src/todoistAPI.ts
+++ src/todoistAPI.ts
@@ -3,7 +3,7 @@ import { Issue } from './types/issue'
 
 export const addTask = async (issue: Issue): Promise<Task> => {
   const api = new TodoistApi(`${process.env.TODOIST_API_TOKEN}`)
-  const result = await api.addTask({ content: `**[${issue.repository?.name}]** ${issue.title} (${issue.url})`})
+  const result = await api.addTask({ content: `**[${issue.repository?.name}]** ${issue.title} (${issue.html_url})`})
   return result;
 }
 
