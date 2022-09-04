a8ec312cbe821e659ed85e815a4979de4f33ffc0
tomotomy <tomo1999724@gmail.com>
2022-09-04 16:38:52 +0900

    :adhesive_bandage: enable to fetch over 30 (limitation: 100)

--- src/githubAPI.ts
+++ src/githubAPI.ts
@@ -3,7 +3,9 @@ import { GetIssuesResponseType } from "./types/issue";
 
 export const fetchIssues = async () => {
   const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-  const response: GetIssuesResponseType = await octokit.rest.issues.list(); 
+  const response: GetIssuesResponseType = await octokit.rest.issues.list({
+    per_page: 100,
+  }); 
   const issues: GetIssuesResponseType['data'] = response.data
   return issues;
 }
